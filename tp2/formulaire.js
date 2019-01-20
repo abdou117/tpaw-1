d5724e4f856a5f8f47315bc4939f3dcf800ecb88
abdou117 <44175410+abdou117@users.noreply.github.com>
2019-01-20 02:34:40 +0100

    Add files via upload

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -1,6 +1,26 @@
-function validation () {
-var nameValue=document.querySelector("#nom").value;
-if(nameValue==""){
-    document.querySelector("#error").textContent="le nom est obligatoire";
-}
+function validation(){
+    var isError = false;
+    var divs = document.querySelectorAll('input.test');
+
+[].forEach.call(divs, function(item) {
+ // do whatever
+ if(item.value.length == 0){
+       document.getElementById("error").innerHTML="La saisie des champs est oblgatoire";
+       isError = true;
+
+   }
+   else if (item.value.length <5) {
+       document.getElementById("error").innerHTML="Les champs doivent contenir au moins 5 caracteres";
+       isError = true;
+   }
+   
+   
+   
+   
+});
+if(isError == false){
+       document.getElementById("div1").style.backgroundColor="green";
+document.getElementById("error").innerHTML="Bienvenue:"  + ""+ document.getElementById("nom").value;
+   }
+
 }
